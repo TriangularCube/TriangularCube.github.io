2f7d308916cdfefdfad5e8beec2620cba120f376
Michael Liu <mikeliu@evidentpoint.com>
2021-01-25 21:46:23 -0800

    Basic layout

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,17 +1,44 @@
-import "../bulma.scss"
-import React from 'react'
+import React, { FC, ReactElement, useState } from 'react'
+import '../bulma.scss'
 
-const Layout = ({ children }) => {
+const Layout: FC = ({ children }): ReactElement => {
+  const [isMenuOpen, setMenuOpen] = useState(false)
+
   return (
     <>
-      <section className='hero'>
-        <div className='hero-body'>
-          <div className="container">
-            <h1>Title</h1>
+      <nav className='navbar is-fixed-top'>
+        <div className='container'>
+          <div className='navbar-brand'>
+            <a className='navbar-item'>
+              <h2 className='is-size-5 is-family-primary has-text-weight-bold'>
+                Triangular Cube
+              </h2>
+            </a>
+
+            <a
+              role='button'
+              className={`navbar-burger ${isMenuOpen && `is-active`}`}
+              aria-label='menu'
+              aria-expanded='false'
+              onClick={() => setMenuOpen(!isMenuOpen)}
+            >
+              <span aria-hidden='true'></span>
+              <span aria-hidden='true'></span>
+              <span aria-hidden='true'></span>
+            </a>
+          </div>
+          <div className={`navbar-menu ${isMenuOpen && `is-active`}`}>
+            <div className='navbar-end'>
+              <a className='navbar-item'>
+                <h4>Blog</h4>
+              </a>
+            </div>
           </div>
         </div>
+      </nav>
+      <section className='section'>
+        <div className='container'>{children}</div>
       </section>
-      {children}
     </>
   )
 }
