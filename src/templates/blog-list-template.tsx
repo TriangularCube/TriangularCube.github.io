f6bad2d4a0e647d97ff0b86250a247750a24af62
Michael Liu <michael@triangularcube.com>
2021-09-01 19:26:24 -0700

    Pagination links

--- src/templates/blog-list-template.tsx
+++ src/templates/blog-list-template.tsx
@@ -17,6 +17,10 @@ interface DataType {
       }[]
     }
   }
+  pageContext: {
+    numPages: number
+    currentPage: number
+  }
 }
 
 const useStyles = makeStyles({
@@ -26,10 +30,26 @@ const useStyles = makeStyles({
     alignItems: 'center',
     padding: '0.5rem 0',
   },
+  navSection: {
+    display: 'flex',
+    flexDirection: 'row',
+    width: '60%',
+    marginTop: '8rem',
+  },
+  spacer: {
+    flex: 1,
+  },
 })
 
-function Blog({ data }: DataType) {
+function Blog({ data, pageContext }: DataType) {
   const classes = useStyles()
+
+  const { currentPage, numPages } = pageContext
+  const isFirst = currentPage === 1
+  const isLast = currentPage === numPages
+  const prevPage =
+    currentPage - 1 === 1 ? '/blog' : (currentPage - 1).toString()
+  const nextPage = (currentPage + 1).toString()
 
   return (
     <>
@@ -47,27 +67,40 @@ function Blog({ data }: DataType) {
             </Typography>
           </article>
         ))}
+        <div className={classes.navSection}>
+          {!isFirst && (
+            <Link to={prevPage} rel='prev'>
+              ← Previous Page
+            </Link>
+          )}
+          <div className={classes.spacer} />
+          {!isLast && (
+            <Link to={nextPage} rel='next'>
+              Next Page →
+            </Link>
+          )}
+        </div>
       </Center>
     </>
   )
 }
 
 export const query = graphql`
-    query ($skip: Int!, $limit: Int!) {
-        allMdx(
-            sort: { fields: frontmatter___date, order: DESC }
-            limit: $limit
-            skip: $skip
-        ) {
-            nodes {
-                frontmatter {
-                    title
-                    date(formatString: "MMMM D, YYYY")
-                }
-                slug
-            }
+  query ($skip: Int!, $limit: Int!) {
+    allMdx(
+      sort: { fields: frontmatter___date, order: DESC }
+      limit: $limit
+      skip: $skip
+    ) {
+      nodes {
+        frontmatter {
+          title
+          date(formatString: "MMMM D, YYYY")
         }
+        slug
+      }
     }
+  }
 `
 
 export default Blog
