ebf23d3d45475255e70fd134b673c47b05ee6a49
Michael Liu <michael@triangularcube.com>
2021-09-05 13:48:41 -0700

    Fix codeblock scrolling issue

--- src/components/CodeBlock.tsx
+++ src/components/CodeBlock.tsx
@@ -14,7 +14,7 @@ export function CodeBlock(props: PropsWithChildren<any>): ReactElement {
       theme={theme}
     >
       {({ className, style, tokens, getLineProps, getTokenProps }) => (
-        <pre className={className} style={{ ...style, padding: '20px', overflow: 'scroll' }}>
+        <pre className={className} style={{ ...style, padding: '20px', overflow: 'auto' }}>
           {tokens.map((line, i) => (
             <div key={i} {...getLineProps({ line, key: i })}>
               {line.map((token, key) => (
