9927b0eebec36eb078b28d9ca2ade746e28db6c0
Michael Liu <michael@triangularcube.com>
2021-09-04 20:54:29 -0700

    Custom 404

---
+++ src/pages/404.tsx
@@ -0,0 +1,11 @@
+import { Typography } from '@material-ui/core'
+import React from 'react'
+import { Center } from '../components/Center'
+
+function ErrorPage() {
+  return <Center>
+    <Typography variant='h4'>Uh oh, it looks like there's been an error</Typography>
+  </Center>
+}
+
+export default ErrorPage
