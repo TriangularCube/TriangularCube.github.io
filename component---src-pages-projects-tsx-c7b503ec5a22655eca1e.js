5f94b0c31b1aa3ffff1b43153625cd87b3bf3d20
TriangularCube <TriangularCube@users.noreply.github.com>
2022-12-02 21:32:39 +0000

    deployed via Gatsby Publish Action 🎩 for e325d40e81e2911f7b26bcb9a06e0a517d0965ae

---
+++ component---src-pages-projects-tsx-c7b503ec5a22655eca1e.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunk=self.webpackChunk||[]).push([[317],{6002:function(e,t,n){n.r(t);var l=n(7294),r=n(5414),c=n(7255);t.default=function(){return l.createElement(l.Fragment,null,l.createElement(r.q,null,l.createElement("title",null,"Projects")),l.createElement(c.M,null,l.createElement("div",{className:"title is-4"},"Under Construction")))}}}]);
+//# sourceMappingURL=component---src-pages-projects-tsx-c7b503ec5a22655eca1e.js.map
